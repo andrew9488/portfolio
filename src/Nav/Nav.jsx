01b90a277d81c133f544bf0cd140a56745c3dc42
andrew9488 <pashkevichandrew@gmail.com>
2021-05-09 18:23:54 +0300

    fix: fix bug

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -5,13 +5,13 @@ import {Link, animateScroll as scroll} from "react-scroll";
 export function Nav() {
     return (
         <div className={style.nav}>
-            <Link to="main" spy={true} smooth={true} offset={-66} duration={700}
+            <Link to="main" spy={true} smooth={true} offset={-71} duration={700}
                   activeClass={style.activeLink}>Main</Link>
-            <Link to="skills" spy={true} smooth={true} offset={-66} duration={700}
+            <Link to="skills" spy={true} smooth={true} offset={-71} duration={700}
                   activeClass={style.activeLink}>Skills</Link>
-            <Link to="projects" spy={true} smooth={true} offset={-66} duration={700}
+            <Link to="projects" spy={true} smooth={true} offset={-71} duration={700}
                   activeClass={style.activeLink}>Projects</Link>
-            <Link to="contacts" spy={true} smooth={true} offset={-64} duration={700}
+            <Link to="contacts" spy={true} smooth={true} offset={-67} duration={700}
                   activeClass={style.activeLink}>Contacts</Link>
         </div>
     );
